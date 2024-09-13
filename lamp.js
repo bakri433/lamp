0f6eebaef8d6210074c7441716a19bfd463ef110
Alnabulsi Perfumes <148901682+bakri433@users.noreply.github.com>
2024-09-13 19:31:19 +0300

    Add files via upload

---
+++ lamp.js
@@ -0,0 +1,11 @@
+document.querySelector(".play .green").onclick=function(){
+    document.querySelector(".play .c-1").classList.toggle("on");
+    document.querySelector(".play .green").classList.toggle("on");
+    document.querySelector(".container .lamp").classList.toggle("on");
+    document.querySelector(".all .lamp .one").classList.toggle("on");
+    document.querySelector(".all .lamp .two").classList.toggle("on");
+    document.querySelector(".all .lamp .three").classList.toggle("on");
+    document.querySelector(".all .lamp .circle1").classList.toggle("on");
+    document.querySelector(".all .lamp .circle2").classList.toggle("on");
+   
+}
